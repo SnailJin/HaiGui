2a0a4650a8c449f0994de2a5a5e2170e5b30a4b8
SnailJin <jin521436@163.com>
2017-04-08 23:28:22 +0800

    个人中心修改

--- static/js/acount.js
+++ static/js/acount.js
@@ -4,12 +4,63 @@
 $(function(){
     $('.user-edit').click(function(){
         rel = $(this).attr('rel');
-        $(this).closest("#"+rel+"-span").hide();
-        $('#'+rel+'-form').show();
+        span = $(this).closest("#"+rel+"-span");
+        span.hide();
+        form = $('#'+rel+'-form');
+        parseForm(span,form)
+        form.show();
     })
     $('.cancel').click(function(){
         rel = $(this).attr('rel');
         $("#"+rel+"-span").show();
         $('#'+rel+'-form').hide();
     })
+    $('.submit').click(function(){
+        form = $(this).closest('form');
+        className = form.attr('id').split('-form')[0]
+        spanDiv = $('#'+className+'-span');
+        paseSpan(spanDiv,form);
+        form.hide();
+        spanDiv.show();
+        return false;
+    })
+    //添加按钮
+    $('.add-btn').click(function(){
+        rel = $(this).attr('rel');
+        form = $('#'+rel+'-form');
+        if(!form.is(':hidden')){
+            return;
+        }
+        form.find('input').html('');
+        form.show();
+    })
 });
+//转成form
+function parseForm(spanDiv,formDiv){
+    spanDiv = span.find("span[class$='_span']");
+    for(var i=0 ;i < spanDiv.length ; i++){
+        content = spanDiv[i];
+        className = content.className;
+        val = $(content).text();
+        formCLassName = className.split('_span')[0];
+        feildForm =formDiv.find('.'+formCLassName);
+        if(feildForm.is('select')){
+
+        }else if(feildForm.is('span')){
+            feildForm.text(val);
+        }else{
+            feildForm.val(val);
+        }
+    }
+}
+//转成span
+function paseSpan(spanDiv,formDiv){
+    formFieldList = formDiv.find("input,select");
+    for(var i=0 ;i < formFieldList.length ; i++){
+        contentForm = formFieldList[i];
+        className = contentForm.getAttribute("name");
+        val = $(contentForm).val();
+        feild =spanDiv.find('.'+className+'_span');
+        feild.text(val)
+    }
+}
